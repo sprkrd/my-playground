e34f6d3746f60df0c71f4c08cfe404c2b2035bfa
Alejandro Suárez Hernández <asuarez@iri.upc.edu>
2022-11-11 12:45:43 +0100

    resize thingy

--- fltk/src/resize.cpp
+++ fltk/src/resize.cpp
@@ -1,24 +1,66 @@
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Box.H>
- 
+#include <FL/Fl_Button.H>
+
+#include <tuple>
+
+typedef std::tuple<int,int> Point2i;
+
+
+int AlignCenter(int parentSize, int size) {
+    return std::max((parentSize-size)/2, 0);
+}
+
+
+Point2i AlignCenter(int parentW, int parentH, int w, int h) {
+    return Point2i(AlignCenter(parentW, w), AlignCenter(parentH, h));
+}
+
+
+class CenteredGroup : public Fl_Group {
+    public:
+        CenteredGroup(int W, int H, const char* l = nullptr) : Fl_Group(0, 0, W, H, l) {
+            auto[X,Y] = AlignCenter(parent()->w(), parent()->h(), W, H);
+            position(X, Y);
+        }
+
+        void resize(int X, int Y, int W, int H) override {
+            int W0 = GetInitialWidth();
+            int H0 = GetInitialHeight();
+            //W = std::max(W, W0);
+            //H = std::max(H, H0);
+            if (W*H0 > H*W0)
+                W = W0 * H/H0;
+            else
+                H = H0 * W/W0;
+            std::tie(X, Y) = AlignCenter(parent()->w(), parent()->h(), W, H);
+            Fl_Group::resize(X, Y, W, H);
+        }
+
+    private:
+
+        int GetInitialWidth() {
+            int* s = sizes();
+            return s[1] - s[0];
+        }
+
+        int GetInitialHeight() {
+            int* s = sizes();
+            return s[3] - s[2];
+        }
+};
+
+
 int main(int argc, char **argv) {
-  Fl_Window *window = new Fl_Window(512, 512);
-  Fl_Box* spacer1 = new Fl_Box(0, 0, 128, 128, nullptr);
-  spacer1->box(FL_FLAT_BOX);
-  spacer1->color(FL_BLUE);
-  Fl_Box* spacer2 = new Fl_Box(384, 384, 128, 128, nullptr);
-  spacer2->box(FL_FLAT_BOX);
-  spacer2->color(FL_RED);
-  Fl_Group* mainViewport = new Fl_Group(128, 128, 256, 256, nullptr);
-  mainViewport->resizable(nullptr);
-  Fl_Box* centralBox = new Fl_Box(128,128,100,100,nullptr);
-  centralBox->box(FL_UP_BOX);
-  mainViewport->end();
-  //mainViewport->position(256, 256);
-  //box->labelfont(FL_BOLD+FL_ITALIC);
-  //box->labelsize(36);
-  //box->labeltype(FL_SHADOW_LABEL);
+  Fl_Window* window = new Fl_Window(512, 512);
+  Fl_Group* myGroup = new CenteredGroup(512, 512);
+
+  new Fl_Button(128, 128, 100, 50, "Hey!");
+
+  myGroup->box(FL_FLAT_BOX);
+  myGroup->color(FL_BLUE);
+  myGroup->end();
   window->end();
   window->resizable(window);
   window->show(argc, argv);
