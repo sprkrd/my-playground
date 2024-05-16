b26e56f0ca32969b1be383db27dce62d53d1a34c
Alejandro Suarez Hernandez <asuarez@iri.upc.edu>
2024-05-16 10:18:16 +0200

    up to part 5

--- django-tutorial/django_tutorial/polls/urls.py
+++ django-tutorial/django_tutorial/polls/urls.py
@@ -4,14 +4,22 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [
-    # e.g.: /polls/
-    path("", views.index, name="index"),
-    # e.g.: /polls/5/
-    path("<int:question_id>/", views.detail, name="detail"),
-    # e.g.: /polls/5/results/
-    path("<int:question_id>/results/", views.results, name="results"),
-    # e.g.: /polls/5/votes/
-    path("<int:question_id>/vote/", views.vote, name="vote"),
+app_name = "polls"
+# urlpatterns = [
+#     # e.g.: /polls/
+#     path("", views.index, name="index"),
+#     # e.g.: /polls/5/
+#     path("<int:question_id>/", views.detail, name="detail"),
+#     # e.g.: /polls/5/results/
+#     path("<int:question_id>/results/", views.results, name="results"),
+#     # e.g.: /polls/5/votes/
+#     path("<int:question_id>/vote/", views.vote, name="vote"),
+#
+# ]
 
+urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
